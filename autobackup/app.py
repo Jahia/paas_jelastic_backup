4a6c3444436925eae82251073ccaab80f89e968b
laurent fuentes <lfuentes@jahia.com>
2019-06-29 10:35:07 +0200

    gogogo

--- autobackup/app.py
+++ autobackup/app.py
@@ -16,7 +16,7 @@ LOG_FORMAT = "%(asctime)s %(levelname)s: [%(funcName)s] %(message)s"
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 
 
-script = "source /.jelenv && cd / && /import_package_as_user.py -l {login} -p {password} -u '{url}' --settings '{settings}' --env {env} --region {region} -s {sudo} >> /var/log/backup_{sudo}-{env}.log 2>&1"
+script = "source /.jelenv && cd / && /import_package_as_user.py -l {login} -p {password} -u '{url}' --settings "{settings}" --env {env} --region {region} -s {sudo} >> /var/log/backup_{sudo}-{env}.log 2>&1"
 
 @app.route("/")
 def hello():
@@ -50,7 +50,7 @@ class CronJob(Resource):
             command = script.format(login='${MASTER_LOGIN}',
                                     password='${MASTER_PWD}',
                                     url=args.url,
-                                    settings= args.settings,
+                                    settings= args.settings.replace("'", '"'),
                                     env=args.envname,
                                     region=args.region,
                                     sudo=args.sudo)
