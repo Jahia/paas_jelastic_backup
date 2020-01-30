2e68672521be046b9382658521f313a0bfa8a432
Ronan GORAIN <rgorain@jahia.com>
2020-01-30 10:15:33 +0100

    PAAS-584: add comments and refactor elasticsearch.py

--- elasticsearch.py
+++ elasticsearch.py
@@ -14,10 +14,6 @@ AZ_CRED = "/tmp/azurecred"
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--accesskey",
-                        help="the cp access key to connect")
-    parser.add_argument("--secretkey",
-                        help="the cp secret key to connect")
     parser.add_argument("--bucketname",
                         help="the bucket name you want to play with",
                         required=False)
@@ -25,8 +21,11 @@ def argparser():
                         help="the backup name you want to play with",
                         required=False)
     parser.add_argument("-c", "--cloudprovider",
-                        help="the backup cloudprovider"
-                        )
+                        help="the backup cloudprovider",
+                        required=True)
+    parser.add_argument("-o", "--operation",
+                        help="backup or restore",
+                        required=True)
     return parser.parse_args()
 
 
@@ -34,6 +33,8 @@ if __name__ == '__main__':
     args = argparser()
     try:
         with open('/metadata_from_HOST', 'r') as f:
+            # Theses infos are required to create AZ or AWS object,
+            # but are not used in case of restore
             props = dict(line.strip().split('=', 1) for line in f)
             region = props['JEL_REGION']
             role = props['JEL_ENV_ROLE']
@@ -41,11 +42,13 @@ if __name__ == '__main__':
         logging.error("A problem occured when reading /metadata_from_HOST file. Exiting")
         exit(1)
 
-    if args.cloudprovider == 'aws':
+    if args.cloudprovider == "aws" and args.operation == "backup":
+        # Create bucket if not exists
         cp = AWS.PlayWithIt(region_name=region, env=role)
         if not cp.test_if_bucket_exist(args.bucketname):
             cp.create_bucket(args.bucketname)
-    else:
+    elif args.cloudprovider == "azure":
+        # Get azure secrets
         aws_sm_md = AWS.PlayWithIt(region_name="eu-west-1")
         cp = AZ.PlayWithIt(region_name=region, sto_cont_name=args.backupname,
                            rg=AZ_RG, sto_account=args.bucketname,
@@ -55,10 +58,14 @@ if __name__ == '__main__':
         secret = json.loads(secret)
         with open(AZ_CRED, 'w') as f:
             f.write(json.dumps(secret, indent=4, sort_keys=True))
-        if not cp.check_if_sto_acc_exist():
-            cp.create_sto_account()
-        if not cp.test_if_obj_exist(object_name=cp.sto_cont_name):
-            cp.create_sto_container()
+
+        if args.operation == "backup":
+            # Check if storage accout exists
+            if not cp.check_if_sto_acc_exist():
+                cp.create_sto_account()
+            if not cp.test_if_obj_exist(object_name=cp.sto_cont_name):
+                cp.create_sto_container()
+
         account_key = cp.get_sto_account_key()
         with open(AZ_CRED, 'w') as f:
             f.write(account_key)
